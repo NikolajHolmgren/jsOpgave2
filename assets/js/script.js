2f306dc1d563c14fb909971ec9427324b9bdfebc
NikolajHolmgren <119925735+NikolajHolmgren@users.noreply.github.com>
2023-03-27 13:21:46 +0200

    Changed background image

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,10 @@ socialElements.forEach(function (socialELement) {
 
 /*3. Brug javascript til at skifte billedet ud i .bigPicture-boksen, brug dette eller noget helt andet: https://cdn.pixabay.com/photo/2017/09/28/13/18/amusement-2795490_960_720.jpg */
 
+const backgroundImage = document.querySelector(".bigPicture")
+
+backgroundImage.style.backgroundImage = "url('https://cdn.pixabay.com/photo/2017/09/28/13/18/amusement-2795490_960_720.jpg'"
+
 /*4. Brug javascript til at indsætte et favicon - f.eks. det her http://hearthstoneapi.com/favicon.ico (I know...hvorfor ville man nogensinde gøre det irl...men for øvelsens skyld) */
 
 /*5.1 Brug javascript til at gemme teksterne væk i artiklerne <p>-tags. Indsæt istedet link med "Læs mere..." */
